eae8cc072a2732cee0b4213003c052a1b1382767
Roman Levishchenko <index.0h@gmail.com>
2015-06-09 23:42:09 +0300

    add coveralls

---
+++ .coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "mode: count" > profile.cov
+
+# Standard go tooling behavior is to ignore dirs with leading underscors
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
+do
+if ls $dir/*.go &> /dev/null; then
+    go test -covermode=count -coverprofile=$dir/profile.tmp $dir
+    if [ -f $dir/profile.tmp ]
+    then
+        cat $dir/profile.tmp | tail -n +2 >> profile.cov
+        rm $dir/profile.tmp
+    fi
+fi
+done
