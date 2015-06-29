ebe123f30851112a7c1da016695150607aa21bb8
Roman Levishchenko <index.0h@gmail.com>
2015-06-30 02:10:09 +0300

    fix event log

--- entities/event_log.go
+++ entities/event_log.go
@@ -5,36 +5,45 @@ import "errors"
 type EventLog struct {
 	eventLogID [16]byte
 	timestamp  int64
-	event      *Event
-	visit      *Visit
-	data       map[string]string
+	visitID    [16]byte
+	visitData  map[string]string
+	eventsData map[[16]byte]map[string]string
 }
 
 func NewEventLog(
 	eventLogID [16]byte,
 	timestamp int64,
-	event *Event,
 	visit *Visit,
-	data map[string]string,
+	events []*Event,
 ) (*EventLog, error) {
 	if eventLogID == [16]byte{} {
 		return nil, errors.New("Empty eventLogID is not allowed")
-	}
-
-	if event == nil {
-		return nil, errors.New("Param event must be not nil")
 	}
 
 	if visit == nil {
 		return nil, errors.New("Param visit must be not nil")
 	}
 
-	copyData := make(map[string]string, len(data))
-	for key, value := range data {
-		copyData[key] = value
+	if events == nil {
+		return nil, errors.New("Param events must be not nil")
 	}
 
-	return &EventLog{eventLogID: eventLogID, timestamp: timestamp, event: event, visit: visit, data: copyData}, nil
+	if len(events) == 0 {
+		return nil, errors.New("Param events must be not empty")
+	}
+
+	eventsData := make(map[[16]byte]map[string]string, len(events))
+	for _, event := range events {
+		eventsData[event.EventID()] = event.Data()
+	}
+
+	return &EventLog{
+		eventLogID: eventLogID,
+		timestamp: timestamp,
+		visitID: visit.visitID,
+		visitData: visit.Data(),
+		eventsData: eventsData,
+	}, nil
 }
 
 func (eventLog *EventLog) EventLogID() [16]byte {
@@ -45,18 +54,27 @@ func (eventLog *EventLog) Timestamp() int64 {
 	return eventLog.timestamp
 }
 
-func (eventLog *EventLog) Event() *Event {
-	return eventLog.event
+func (eventLog *EventLog) VisitID() [16]byte {
+	return eventLog.visitID
 }
 
-func (eventLog *EventLog) Visit() *Visit {
-	return eventLog.visit
-}
+func (eventLog *EventLog) VisitData() map[string]string {
+	result := make(map[string]string, len(eventLog.visitData))
 
-func (eventLog *EventLog) Data() map[string]string {
-	result := make(map[string]string, len(eventLog.data))
-	for key, value := range eventLog.data {
+	for key, value := range eventLog.visitData {
 		result[key] = value
+	}
+
+	return result
+}
+
+func (eventLog *EventLog) EventsData() map[[16]byte]map[string]string {
+	result := make(map[[16]byte]map[string]string, len(eventLog.eventsData))
+
+	for uuid, data := range eventLog.eventsData {
+		for key, value := range data {
+			result[uuid][key] = value
+		}
 	}
 
 	return result
--- entities/event_log_test.go
+++ entities/event_log_test.go
@@ -8,63 +8,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_EventLog_NewEventLog(t *testing.T) {
-	eventLogID := uuid.New().Generate()
-	timestamp := time.Now().Unix()
-	visit := &Visit{}
-	event := &Event{}
-	data := map[string]string{"extra": "data"}
-
-	eventLog, err := NewEventLog(eventLogID, timestamp, event, visit, data)
-
-	assert.NotNil(t, eventLog)
-	assert.Nil(t, err)
-	assert.Equal(t, eventLogID, eventLog.EventLogID())
-	assert.Equal(t, timestamp, eventLog.Timestamp())
-	assert.Equal(t, event, eventLog.Event())
-	assert.Equal(t, visit, eventLog.Visit())
-	assert.Equal(t, data, eventLog.Data())
-}
-
 func Test_EventLog_NewEventLog_EmptyEventLogID(t *testing.T) {
-	visit := &Visit{}
-	event := &Event{}
-
-	eventLog, err := NewEventLog([16]byte{}, time.Now().Unix(), event, visit, map[string]string{})
+	eventLog, err := NewEventLog([16]byte{}, time.Now().Unix(), &Visit{}, []*Event{&Event{}})
 
 	assert.Nil(t, eventLog)
 	assert.NotNil(t, err)
 }
 
 func Test_EventLog_NewEventLog_EmptyEvent(t *testing.T) {
-	visit := &Visit{}
-
-	eventLog, err := NewEventLog(uuid.New().Generate(), time.Now().Unix(), nil, visit, map[string]string{})
+	eventLog, err := NewEventLog(uuid.New().Generate(), time.Now().Unix(), nil, []*Event{&Event{}})
 
 	assert.Nil(t, eventLog)
 	assert.NotNil(t, err)
 }
 
 func Test_EventLog_NewEventLog_EmptyVisit(t *testing.T) {
-	event := &Event{}
-
-	eventLog, err := NewEventLog(uuid.New().Generate(), time.Now().Unix(), event, nil, map[string]string{})
+	eventLog, err := NewEventLog(uuid.New().Generate(), time.Now().Unix(), &Visit{}, []*Event{})
 
 	assert.Nil(t, eventLog)
 	assert.NotNil(t, err)
-}
-
-func Test_EventLog_Data_Copy(t *testing.T) {
-	data := map[string]string{"A": "B"}
-
-	eventLogID := uuid.New().Generate()
-	timestamp := time.Now().Unix()
-	visit := &Visit{}
-	event := &Event{}
-
-	eventLog, err := NewEventLog(eventLogID, timestamp, event, visit, data)
-
-	data["B"] = "C"
-	assert.Nil(t, err)
-	assert.NotEqual(t, data, eventLog.Data())
 }
