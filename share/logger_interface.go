da9b4f1738d35166152dd8179f134e6ff895ea02
Roman Levishchenko <index.0h@gmail.com>
2015-09-07 03:24:14 +0300

    fix logger interface

--- share/logger_interface.go
+++ share/logger_interface.go
@@ -1,9 +1,9 @@
 package share
 
 type LoggerInterface interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
-	Fatal(args ...interface{})
+	Debug(args ...interface{}) LoggerInterface
+	Info(args ...interface{}) LoggerInterface
+	Warn(args ...interface{}) LoggerInterface
+	Error(args ...interface{}) LoggerInterface
+	Fatal(args ...interface{}) LoggerInterface
 }
